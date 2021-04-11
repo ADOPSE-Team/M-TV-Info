6e318abbde414920247db692fad7b9100aeff179
SeijinD <seijindemon@protonmail.com>
2021-04-11 23:21:27 +0300

    add basic layouts and classes for identity

---
+++ M-TV-Info/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using M_TV_Info.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(M_TV_Info.Areas.Identity.IdentityHostingStartup))]
+namespace M_TV_Info.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
