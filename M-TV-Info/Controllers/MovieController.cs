5a6dfe97f5a0cc48122c7d62c9da8c08751be262
Kristel Lamaj <kri.lamaj@gmail.com>
2021-04-26 23:56:46 +0300

    Get Movie  | View & Controller

---
+++ M-TV-Info/Controllers/MovieController.cs
@@ -0,0 +1,35 @@
+using System.Net.Http;
+using System.Threading.Tasks;
+using API.Helpers;
+using M_TV_Info.Models.TMDbModels;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace M_TV_Info.Controllers
+{
+    public class MovieController : Controller
+    {
+        // Return View
+        public IActionResult Movie(int id){
+            
+            var model = GetMovie(id);
+
+            return View(model);
+        }
+        
+        private async Task<MovieModel> GetMovie(int id)
+        {
+            HttpClient http = new HttpClient();
+
+            var data = http.GetAsync("https://api.themoviedb.org/3/movie/" + id + "?api_key=" + Constants.ApiKey);
+
+            if (!(data is null))
+            {
+                var content = await data.Result.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<MovieModel>(content);
+            }
+
+            return null;
+        }
+    }
+}
