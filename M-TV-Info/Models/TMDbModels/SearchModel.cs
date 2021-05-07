6c6424befea0d034c6867ab2992b980498062ffb
Kristel Lamaj <kri.lamaj@gmail.com>
2021-05-07 23:51:46 +0300

    Updated Search Model

--- M-TV-Info/Models/TMDbModels/SearchModel.cs
+++ M-TV-Info/Models/TMDbModels/SearchModel.cs
@@ -5,8 +5,20 @@ namespace M_TV_Info.Models.TMDbModels
     // SearchModel jsonDeserialize = JsonConvert.DeserializeObject<SearchModel>(json); 
     public class SearchResult
     {
-        public string name { get; set; }
+        public bool adult { get; set; }
+        public string backdrop_path { get; set; }
+        public List<int> genre_ids { get; set; }
         public int id { get; set; }
+        public string original_language { get; set; }
+        public string original_title { get; set; }
+        public string overview { get; set; }
+        public double popularity { get; set; }
+        public string poster_path { get; set; }
+        public string release_date { get; set; }
+        public string title { get; set; }
+        public bool video { get; set; }
+        public double vote_average { get; set; }
+        public int vote_count { get; set; }
     }
 
     public class SearchModel
