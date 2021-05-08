358aab1f0629ccea082da9285667e2d3dc3edc53
Kristel Lamaj <kri.lamaj@gmail.com>
2021-05-09 00:33:07 +0300

    Update Contorller

--- M-TV-Info/Controllers/HomeController.cs
+++ M-TV-Info/Controllers/HomeController.cs
@@ -88,11 +88,11 @@ namespace M_TV_Info.Controllers
             var media = JsonConvert.DeserializeObject<FavouriteModelPost>(obj.ToString());
 
             FavouriteModel fav = new FavouriteModel();
-            User usr = await GetCurrentUserAsync();
+            // User usr = await GetCurrentUserAsync();
             DateTime date = DateTime.Now;
 
             fav.media_id = media.media_id;
-            fav.user_id = usr.Id;
+            fav.user_id = "1";
             fav.w_date = date;
 
             if (!(fav is null))
@@ -106,9 +106,9 @@ namespace M_TV_Info.Controllers
                 throw new ArgumentNullException(nameof(fav));
             }
         }
-        
+
         // Get Current USER
-        private Task<User> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+        // private Task<User> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
