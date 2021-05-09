7ddb02306ddcfb7f2f2a3b0ce2a7834650d5cbc9
Ioatsi <j.tsilikas.j@gmail.com>
2021-05-09 15:20:20 +0300

    Refine Movie View

---
+++ M-TV-Info/wwwroot/js/favorites.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+    $('#addFav').click(function () {
+        $.ajax({
+            type: "POST", //HTTP POST Method
+            url: "Home/AddToFavourites", // Controller/View
+            data: { //Passing data
+                media_id: $('#movie_id').val(),
+                movie_title: $('#movie_title').val(),
+                movie_poster: $('#poster_path').val()
+            },
+            success: function (data) {
+                var Movie = $('#movie_title').val();
+                alert("Movie" + Movie + "has been added to favourites list");
+            }
+        });
+    });
+});
