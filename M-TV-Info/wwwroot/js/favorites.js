1f71d3f92c6e59d4f9ca4371fb774a7af93c9c34
SeijinD <seijindemon@protonmail.com>
2021-05-16 18:27:36 +0300

    fix favourite

--- M-TV-Info/wwwroot/js/favorites.js
+++ M-TV-Info/wwwroot/js/favorites.js
@@ -20,9 +20,10 @@ $(document).ready( function() {
             data: _item,
             //contentType: "application/json; charset=utf-8",
             success: function () {
+                
             }
         });
 
-        location.reload();
+        
     });
 });
